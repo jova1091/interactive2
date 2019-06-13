f5f5a82ceca8c081fab41b7f7f03f970619215e8
Oscar Perez <ospersa@gmail.com>
2019-06-13 17:04:09 -0500

    Desarrollo completo btn 2

--- js/master.js
+++ js/master.js
@@ -46,7 +46,46 @@ document.addEventListener('DOMContentLoaded', function () {
     var insSlider3_4 = M.Slider.getInstance(slider3_4);
     insSlider3_4.pause();
 
+    document.querySelector('.modal2-1-1-6_P0').onclick = function() {btnModal2_1_1_6_1()};
+    document.querySelector('.modal2-1-1-6_P1').onclick = function() {btnModal2_1_1_6_2()};
+    document.querySelector('.modal2-1-1-6_P2').onclick = function() {btnModal2_1_1_6_3()};
+
+
 });
+
+function btnModal2_1_1_6_1() {
+    var elem = document.getElementById("imgModal2-1-1-6-1");
+    elem.classList.remove("pulse");
+    var text = document.getElementById("modal2-1-1-6_P2");
+    text.style.display = "none";
+    var text1 = document.getElementById("modal2-1-1-6_P0");
+    text1.style.display = "table";
+    var elem1 = document.getElementById("imgModal2-1-1-6-2");
+    elem1.classList.add("pulse");
+}
+
+
+function btnModal2_1_1_6_2() {
+    var elem = document.getElementById("imgModal2-1-1-6-2");
+    elem.classList.remove("pulse");
+    var text = document.getElementById("modal2-1-1-6_P0");
+    text.style.display = "none";
+    var text1 = document.getElementById("modal2-1-1-6_P1");
+    text1.style.display = "table";
+    var elem1 = document.getElementById("imgModal2-1-1-6-3");
+    elem1.classList.add("pulse");
+}
+
+function btnModal2_1_1_6_3() {
+    var elem = document.getElementById("imgModal2-1-1-6-3");
+    elem.classList.remove("pulse");
+    var text = document.getElementById("modal2-1-1-6_P1");
+    text.style.display = "none";
+    var text1 = document.getElementById("modal2-1-1-6_P2");
+    text1.style.display = "table";
+    var elem1 = document.getElementById("imgModal2-1-1-6-1");
+    elem1.classList.add("pulse");
+}
 
 function btnModal1(){
     var elem = document.getElementById("btnModal1");
@@ -107,16 +146,92 @@ function btnModal2_3() {
 }
 
 function btnModal2_1_1_1() {
-    var elem = document.getElementById("imgModal2-1-1-1");
-    elem.classList.remove("pulse");
+    var elem_1 = document.getElementById("imgModal2-1-1-1");
+    var elem_2 = document.getElementById("imgModal2-1-1-3");
+    var elem_3 = document.getElementById("imgModal2-1-1-4");
+    var elem_4 = document.getElementById("imgModal2-1-1-5");
+    var elem_5 = document.getElementById("imgModal2-1-1-6");
+    elem_1.classList.remove("pulse");
+    elem_2.classList.remove("pulse");
+    elem_3.classList.remove("pulse");
+    elem_4.classList.remove("pulse");
+    elem_5.classList.remove("pulse");
     var elem1 = document.getElementById("imgModal2-1-1-2");
     elem1.classList.add("pulse");
 }
 
 function btnModal2_1_1_2() {
-    var elem = document.getElementById("imgModal2-1-1-2");
-    elem.classList.remove("pulse");
+    var elem_1 = document.getElementById("imgModal2-1-1-2");
+    var elem_2 = document.getElementById("imgModal2-1-1-4");
+    var elem_3 = document.getElementById("imgModal2-1-1-5");
+    var elem_4 = document.getElementById("imgModal2-1-1-6");
+    var elem_5 = document.getElementById("imgModal2-1-1-1");
+    elem_1.classList.remove("pulse");
+    elem_2.classList.remove("pulse");
+    elem_3.classList.remove("pulse");
+    elem_4.classList.remove("pulse");
+    elem_5.classList.remove("pulse");
     var elem1 = document.getElementById("imgModal2-1-1-3");
+    elem1.classList.add("pulse");
+}
+
+function btnModal2_1_1_3() {
+    var elem_1 = document.getElementById("imgModal2-1-1-3");
+    var elem_2 = document.getElementById("imgModal2-1-1-5");
+    var elem_3 = document.getElementById("imgModal2-1-1-6");
+    var elem_4 = document.getElementById("imgModal2-1-1-1");
+    var elem_5 = document.getElementById("imgModal2-1-1-2");
+    elem_1.classList.remove("pulse");
+    elem_2.classList.remove("pulse");
+    elem_3.classList.remove("pulse");
+    elem_4.classList.remove("pulse");
+    elem_5.classList.remove("pulse");
+    var elem1 = document.getElementById("imgModal2-1-1-4");
+    elem1.classList.add("pulse");
+}
+
+function btnModal2_1_1_4() {
+    var elem_1 = document.getElementById("imgModal2-1-1-4");
+    var elem_2 = document.getElementById("imgModal2-1-1-6");
+    var elem_3 = document.getElementById("imgModal2-1-1-1");
+    var elem_4 = document.getElementById("imgModal2-1-1-2");
+    var elem_5 = document.getElementById("imgModal2-1-1-3");
+    elem_1.classList.remove("pulse");
+    elem_2.classList.remove("pulse");
+    elem_3.classList.remove("pulse");
+    elem_4.classList.remove("pulse");
+    elem_5.classList.remove("pulse");
+    var elem1 = document.getElementById("imgModal2-1-1-5");
+    elem1.classList.add("pulse");
+}
+
+function btnModal2_1_1_5() {
+    var elem_1 = document.getElementById("imgModal2-1-1-5");
+    var elem_2 = document.getElementById("imgModal2-1-1-1");
+    var elem_3 = document.getElementById("imgModal2-1-1-2");
+    var elem_4 = document.getElementById("imgModal2-1-1-3");
+    var elem_5 = document.getElementById("imgModal2-1-1-4");
+    elem_1.classList.remove("pulse");
+    elem_2.classList.remove("pulse");
+    elem_3.classList.remove("pulse");
+    elem_4.classList.remove("pulse");
+    elem_5.classList.remove("pulse");
+    var elem1 = document.getElementById("imgModal2-1-1-6");
+    elem1.classList.add("pulse");
+}
+
+function btnModal2_1_1_6() {
+    var elem_1 = document.getElementById("imgModal2-1-1-6");
+    var elem_2 = document.getElementById("imgModal2-1-1-2");
+    var elem_3 = document.getElementById("imgModal2-1-1-3");
+    var elem_4 = document.getElementById("imgModal2-1-1-4");
+    var elem_5 = document.getElementById("imgModal2-1-1-5");
+    elem_1.classList.remove("pulse");
+    elem_2.classList.remove("pulse");
+    elem_3.classList.remove("pulse");
+    elem_4.classList.remove("pulse");
+    elem_5.classList.remove("pulse");
+    var elem1 = document.getElementById("imgModal2-1-1-1");
     elem1.classList.add("pulse");
 }
 
